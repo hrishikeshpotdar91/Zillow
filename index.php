0ea20d7343b856541b5170ad110c777acd90e801
hrishi <hrishikesh.potdar91@gmail.com>
2015-03-24 13:59:55 -0500

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $address  = preg_replace("/[\s_]/", "+", $address);
 $address  = str_replace(",+United+States", "", $address);
 $geocode  = new Geocode($address);
 $postcode = $geocode->getPostCode();
-$url   = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1eppojqt53f_9od0r&address=$address&citystatezip=$postcode";
+$url   = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=APIKEY&address=$address&citystatezip=$postcode";
 $json  = json_encode(simplexml_load_string(file_get_contents($url)));
 $jsond = json_decode($json);
 echo $jsond->response->results->result->zestimate->amount;

bd6a62edd1b21a7f368574dea17a0094ceaa36e1
hrishi <hrishikesh.potdar91@gmail.com>
2015-03-24 12:59:24 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require("Geocode.php");
+use kamranahmedse\Geocode;
+
+$address  = "2323 Smith Street, Houston, TX, United States";
+$address  = preg_replace("/[\s_]/", "+", $address);
+$address  = str_replace(",+United+States", "", $address);
+$geocode  = new Geocode($address);
+$postcode = $geocode->getPostCode();
+$url   = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1eppojqt53f_9od0r&address=$address&citystatezip=$postcode";
+$json  = json_encode(simplexml_load_string(file_get_contents($url)));
+$jsond = json_decode($json);
+echo $jsond->response->results->result->zestimate->amount;
+
+?>
